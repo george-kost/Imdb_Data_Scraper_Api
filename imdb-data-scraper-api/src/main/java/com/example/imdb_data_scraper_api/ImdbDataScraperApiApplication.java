8e25b109b41fde27fca354c3ce5df8d949054b1d
george-kost <67106410+george-kost@users.noreply.github.com>
2024-07-24 23:31:09 +0300

    Add files via upload

---
+++ imdb-data-scraper-api/src/main/java/com/example/imdb_data_scraper_api/ImdbDataScraperApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imdb_data_scraper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbDataScraperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbDataScraperApiApplication.class, args);
+	}
+
+}
